9462605edffd4d403eff4cac21468db40e7dd895
Paulo Soares <ph665361@gmail.com>
2025-08-06 15:48:21 -0300

    reajustes-2
    mudanças no layout de algumas páginas; adição de um botão para 'limpar carrinho' do cliente; a função do carrinho foi reajustada para receber uma lista de produtos ou apenas um produto por 'compra direta'; uma barra de pesquisa para procurar por produtos; a página de 'index' foi substituida pela página de produtos para uma melhor navegação.

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -7,8 +7,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
 INSERT INTO usuarios (username, senha_hash)
 VALUES ('admin', '$2b$10$cRGfwvfTAsFVjM3iD3WGae6YiCEEp5o5469NeSjpGXcFWdHq150/6');
 
-<<<<<<< Updated upstream
-=======
 -- Categorias
 CREATE TABLE IF NOT EXISTS categorias (
   nome TEXT PRIMARY KEY NOT NULL,
@@ -17,20 +15,15 @@ CREATE TABLE IF NOT EXISTS categorias (
 
 INSERT INTO categorias (nome) VALUES ('doces');
 
->>>>>>> Stashed changes
 -- Produtos
 CREATE TABLE IF NOT EXISTS produtos (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL,
   preco NUMERIC(10,2) NOT NULL,
-<<<<<<< Updated upstream
-  imagem_url TEXT
-=======
   imagem_url TEXT,
   descricao VARCHAR(255),
   nome_categoria TEXT,
   FOREIGN KEY (nome_categoria) REFERENCES categorias(nome)
->>>>>>> Stashed changes
 );
 
 -- Encomendas
